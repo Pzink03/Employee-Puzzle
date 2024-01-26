616f070f712eb76a70b1736635154875af689f00
Pete <pete.zink03@gmail.com>
2024-01-25 20:09:09 -0500

    Update deploy

--- api/src/setup/cors.ts
+++ api/src/setup/cors.ts
@@ -1,7 +1,13 @@
-import cors from "cors"
-import { Express } from "express"
-import { env } from "../config"
+import cors from "cors";
+import { Express } from "express";
+import { env } from "../config";
 
 export function setupCors(app: Express) {
-  app.use(cors({ origin: env.CLIENT_URL, credentials: true, allowedHeaders: '*' }))
+  app.use(
+    cors({
+      origin: env.CLIENT_URL,
+      credentials: true,
+      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    })
+  );
 }
